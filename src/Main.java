4995ad8fc8b39812a6287c25e95359773cd4a5af
Vu Dang Hoang <vdhoang240706@gmail.com>
2025-09-10 10:09:16 +0300

    c

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,45 @@ Task1:
     - Outputs: consultations, treated patients, immunisations.
     - Outcomes: improved community health, reduced disease burden.
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
+Task2:
+  General categories:
+    - Customer-centred (satisfaction, access, fairness)
+    - Service process-centred (waiting time, timeliness, error rates)
+    - Resource-centred (cost, staff productivity, utilisation)
+    - Output-centred (volume of services delivered)
+    - Outcome-centred (impact on well-being, effectiveness)
+    - Sustainability/strategic (future readiness, adaptability)
+
+Task4:
+INITIALISE simulation
+WHILE simulation not finished DO
+    // Phase A
+    ADVANCE simulation time to next scheduled event
+
+    // Phase B
+    PROCESS all events that occur at this time
+
+    // Phase C
+    UPDATE system conditions
+    SCHEDULE any new future events
+    RECORD statistics
+END WHILE
+REPORT results
+
+Task5:
+    // A-phase
+Event nextEvent = eventList.peek();
+clock = nextEvent.getTime();
+
+    // B-phase
+while (eventList.hasEventAt(clock)) {
+    Event e = eventList.removeNext(clock);
+    e.process(systemState, eventList, statistics);
+}
+
+    // C-phase
+systemState.update();
+List<Event> newEvents = systemState.generateFutureEvents(clock);
+for (Event e : newEvents) {
+    eventList.insert(e);
 }
